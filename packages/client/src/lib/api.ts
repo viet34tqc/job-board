ece279e5d90ffe97179c639b235d2b5d345bcee2
viet34tqc <viet34tqc@gmail.com>
2025-06-25 23:44:25 +0700

    Update axios baseUrl

--- packages/client/src/lib/api.ts
+++ packages/client/src/lib/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: import.meta.env.VITE_BACKEND_URL
+    ? `${import.meta.env.VITE_BACKEND_URL}/api/v1`
+    : 'http://localhost:3000/api/v1',
 });
