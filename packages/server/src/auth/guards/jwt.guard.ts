089df986b1833dae93ea4f4f905194fcbab0c9e8
viet nguyen <viet34tqc@gmail.com>
2025-09-09 23:19:17 +0700

    Authorize by permission

--- packages/server/src/auth/guards/jwt.guard.ts
+++ packages/server/src/auth/guards/jwt.guard.ts
@@ -1,7 +1,14 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { AuthUser } from '../auth.type';
 import { IS_PUBLIC_KEY } from '../decoratots/auth.decorator';
 
 @Injectable()
@@ -22,5 +29,29 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       return true;
     }
     return super.canActivate(context);
+  }
+
+  // Hacking
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+    if (err || !user) {
+      throw err || new UnauthorizedException('Invalid token');
+    }
+
+    const request: Request = context.switchToHttp().getRequest();
+    const apiPath = (request.route as { path: string })?.path;
+    const method = request.method;
+
+    const permissions = (user as unknown as AuthUser).permissions ?? [];
+    const userHasPermission = permissions.some(
+      (permission) =>
+        permission.apiPath === apiPath && permission.method === method,
+    );
+
+    if (!userHasPermission) {
+      throw new ForbiddenException('Insufficient permissions');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return user;
   }
 }
