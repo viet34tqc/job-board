51ed55bc347f3288b6d20ca1634d65904bd1cb6e
viet nguyen <viet34tqc@gmail.com>
2025-09-14 17:35:52 +0700

    Test job template

--- packages/server/src/mail/mail.controller.ts
+++ packages/server/src/mail/mail.controller.ts
@@ -13,7 +13,24 @@ export class MailController {
       to: 'viet34tqc@gmail.com',
       from: '"Viet Nguyen" <viet34tqc@gmail.com>',
       subject: 'test mail',
-      template: 'mail',
+      template: 'jobs',
+      context: {
+        subject: 'New Job Opportunities', // This will be available as {{subject}} in the template
+        jobs: [
+          {
+            title: 'Senior Software Engineer', // This will be available as {{this.title}} in the template
+            company: 'Tech Corp',
+            type: 'Full-time',
+            location: 'Remote',
+            salary: '$80,000 - $100,000',
+            description:
+              'We are looking for an experienced software engineer...',
+            applyUrl: 'https://example.com/apply/123',
+          },
+        ],
+        currentYear: new Date().getFullYear(),
+        companyName: 'Your Company',
+      },
     });
   }
 }
